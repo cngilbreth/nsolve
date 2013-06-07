d54214fd10599a64655357aec63780ff32ffcbaa
Christopher N. Gilbreth <cngilbreth@gmail.com>
2013-06-07 11:37:18 -0400

    Factor out one-sided method into solve0.f90

--- src/solve0.f90
+++ src/solve0.f90
@@ -3,36 +3,33 @@ program test
   integer,  parameter :: rk = kind(1d0)
   real(rk), parameter :: pi = 3.141592653589793_rk
 
-  real(rk), parameter :: h = 1./256
+  real(rk), parameter :: dr = 1./256
   real(rk), parameter :: xub = 8._rk
   real(rk), parameter :: xlb = 0._rk
   integer,  parameter :: l = 1
-  integer,  parameter :: n = nint((xub-xlb)/h) + 1
-  real(rk), parameter :: t = 1.e-10_rk
 
-  real(rk) :: v(0:n), u(0:n), x, elb, eub, e, g
+  real(rk) :: v(0:n), u(0:n), xlb, xub, h, x, elb, eub, e, f, g
+  real(rk) :: t = 1d-10
   integer :: i
 
-
+  ! Bounds
+  xlb = 0._rk; xub = 8._rk
+  h = (xub-xlb)/n
   ! Compute v(x)
-  ! Must include effective potential from orbital angular momentum here
   do i=0,n
      x = xlb + i*h
      v(i) = 0.5d0 * x**2 + 0.5d0 * l*(l+1)/(x+t)**2
   end do
   g = 0.5d0
-
-  ! Initial values (approximate)
+  ! Initial values
   u(0) = 0.d0
   u(1) = h**(l+1)
   u(n) = 0.d0
   u(n-1) = exp(-(xub)**2/2)
-
-  ! Eigenvalue window
   elb = 2.25d0
   eub = 2.625d0
 
-  call solve1(h,g,v,elb,eub,u,e)
+  call solve0(h,g,v,elb,eub,u,e)
   open(unit=10,file='u.dat')
   do i=1,n
      write (10,*) i, u(i)
@@ -42,26 +39,7 @@ program test
 contains
 
 
-  subroutine solve1(h,G,v,elb,eub,u,e)
-    ! Find an eigenvalue e and eigenfunction u of the radial Schrodinger
-    ! equation with effective potential v:
-    !   -G u''(x) + (v(x)-e)u = 0,  x ≥ 0, u(0) = u0, u(inf) = uinf
-    ! Input:
-    !   h:   Grid step size
-    !   G:   Multiplier of d²u/dx² term
-    !   v:   Effective potential, evaluated at grid points (must include
-    !        ℏ² l(l+1)/(2 m r^2) term)
-    ! Input/output:
-    !   elb, eub:
-    !        On input: Initial upper and lower bounds for the eigenvalue. The
-    !        [elb,eub] Must bracket exactly 1 eigenvalue, and u must have the
-    !        same number of nodes on this interval.
-    !        On output: A small interval [elb,eub] containing the eigenvalue
-    !   u:   On input, u(0), u(1), u(n-1), and u(n) are set to appropriate
-    !        values for starting the integrations.
-    !        On output, the wavefunction evaluated at the grid points.
-    ! Output:
-    !   e:   Eigenvalue estimate, e = (elb + eub)/2
+  subroutine solve0(h,G,v,elb,eub,u,e)
     implicit none
     real(rk), intent(in) :: h, G, v(:)
     real(rk), intent(inout) :: elb, eub, u(:)
@@ -71,24 +49,24 @@ contains
 
     real(rk) :: fmid, emid, flb, fub
 
-    call fnumerov1(h,g,v,elb,u,flb)
+    call fnumerov0(h,g,v,elb,-1,u,flb)
     open(unit=10,file='ulb.dat')
     do i=1,n
        write (10,*) i, u(i)
     end do
     close(10)
-    call fnumerov1(h,g,v,eub,u,fub)
+    call fnumerov0(h,g,v,eub,-1,u,fub)
     open(unit=11,file='uub.dat')
     do i=1,n
        write (11,*) i, u(i)
     end do
     close(11)
-    if (.not. flb*fub < 0._rk) stop "solve1: Zero not properly bracketed"
+    if (.not. flb*fub < 0._rk) stop "solve: Zero not properly bracketed"
 
     do while (abs(eub - elb) > accuracy)
        ! Try the midpoint in [xlb,xub]
        emid = (elb + eub) * 0.5_rk
-       call fnumerov1(h,g,v,emid,u,fmid)
+       call fnumerov0(h,g,v,emid,-1,u,fmid)
        write (0,*) elb, eub, fmid
        ! Decrease interval
        if (fmid*flb >= 0._rk) then
@@ -102,62 +80,58 @@ contains
     !   1. emid = (xlb + xub)/2
     !   2. abs(eub - elb) < accuracy
     !   3. sign(f(elb)) .ne. sign(f(eub))
-  end subroutine solve1
+  end subroutine solve0
 
 
 
-  subroutine fnumerov1(h,G,v,e,u,f)
+
+
+  subroutine fnumerov0(h,G,v,e,dir,u,f)
     ! Numerically intergrate the homogeneous differential eigenvalue equation,
     !   -G u''(x) + (v(x)-e)u = 0, u(0) = u0, u(inf) = uinf
-    ! from two sides, and compute the fitness function f associated with
-    ! matching the two solutions at a single point.
+    ! from one side only, and compute a fitness function f associated with the
+    ! boundary condition.
     ! Input:
     !   h:       Grid step size (x(i+1)-x(i))
     !   G:       Arbitrary constant
     !   v:       Points v(x(i)), i=1,..,n
     !   e:       Trial eigenvalue
     ! Input/output:
-    !   u:       On input, u(0), u(1), u(n-1), and u(n) are set to appropriate
-    !            values for starting the integrations.
-    !            On output, the two numerically integrated functions, rescaled
-    !            so they match at the separation point.
+    !   u:       On input, u(1), u(n) are set to appropriate boundary values.
+    !            If dir = +1, u(2) is set to an initial value to begin the
+    !            integration.
+    !            If dir = -1, u(n-1) is set to an initial value to begin
+    !            the integration.
+    !            On output, u(1:n-2) or (3:n) is the numerically integrated
+    !            function. The input points are left unchanged.
     ! Notes:
-    !   1. u is a solution to the equation with eigenvalue e when the fitness
-    !      function f is zero (i.e., changes sign).
-    !   2. This routine uses a 5-point formula for the numerical derivative.
+    !   u is a solution to the equation with eigenvalue e when the fitness
+    !   function f is zero (i.e., changes sign).
     implicit none
     real(rk), intent(in) :: h, G, v(:), e
+    integer,  intent(in) :: dir
     real(rk), intent(inout) :: u(:)
     real(rk), intent(out) :: f
 
-    real(rk) :: q(size(v)), S(size(v)), up1, up2, usave(3)
-    integer :: i,n,isep
+    real(rk) :: q(size(v)), S(size(v)), u0, un
 
-    n = size(v)
-    isep = 0
-    do i=1,n
-       q(i) = (e - v(i))/G
-       s(i) = 0.d0
-       ! Select leftmost turning point
-       if (q(i)*q(1) < 0 .and. isep == 0) isep = i
-    end do
-    if (isep == 0) stop "No turning point found"
 
-    call numerov(h,q(1:isep+2),S(1:isep+2),+1,u(1:isep+2))
-    u(1:isep+2) = u(1:isep+2)/u(isep)
-    ! Numerical derivative
-    up1 = (u(isep-2) - 8*u(isep-1) + 8*u(isep+1)-u(isep+2))/(12*h)
-    usave(1:3) = u(isep-2:isep)
-    call numerov(h,q(isep-2:n),S(isep-2:n),-1,u(isep-2:n))
-    u(isep-2:n) = u(isep-2:n)/u(isep)
-    ! Numerical derivative
-    up2 = (u(isep-2) - 8*u(isep-1) + 8*u(isep+1)-u(isep+2))/(12*h)
-    u(isep-2:isep) = usave(1:3)
-
-    ! Compute matching condition
-    ! f = u'_<(x0)/u_<(x0) - u'_>(x0)/u_>(x0)
-    f = up1 - up2
-  end subroutine fnumerov1
+    q = (e - v)/G
+    S = 0
+    if (dir < 0) then
+       u0 = u(1)
+       call numerov(h,q,S,dir,u)
+       !u(1:n-2) = u(1:n-2) / sqrt(dot_product(u(1:n-2),u(1:n-2)))
+       f = u(1)
+       u(1) = u0
+    else
+       un = u(n)
+       call numerov(h,q,S,dir,u)
+       !u(3:n-1) = u(3:n-1) / sqrt(dot_product(u(3:n-1),u(3:n-1)))
+       f = u(n)
+       u(n) = un
+    end if
+  end subroutine fnumerov0
 
 
 
